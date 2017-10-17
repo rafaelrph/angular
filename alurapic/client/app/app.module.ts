b20bc0693c0ce936aeac949f21b39c14f518e096
Rafael Barros <rafael.rph7@gmail.com>
2017-10-17 11:08:19 -0300

    PainelComponent

--- alurapic/client/app/app.module.ts
+++ alurapic/client/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { FotoModule } from './foto/foto.module';
+import { PainelModule } from './painel/painel.module';
 
 @NgModule({
-    imports: [BrowserModule, FotoModule, HttpModule],
+    imports: [BrowserModule, FotoModule, PainelModule, HttpModule],
     declarations: [AppComponent],
     bootstrap: [AppComponent]
 })
