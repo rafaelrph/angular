bfde75d9c881b0a4a27cb0ba66e5030b1bcf28ad
Rafael Barros <rafael.rph7@gmail.com>
2017-10-18 10:19:41 -0300

    Saving on the api - ngoninit

--- alurapic/client/app/cadastro/cadastro.component.ts
+++ alurapic/client/app/cadastro/cadastro.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FotoComponent } from '../foto/foto.component';
+import { Http, Headers } from '@angular/http';
 
 @Component({
     moduleId: module.id,
@@ -9,10 +10,22 @@ import { FotoComponent } from '../foto/foto.component';
 export class CadastroComponent {
 
     foto: FotoComponent = new FotoComponent();
+    http: Http;
+
+    constructor(http: Http) {
+        this.http = http;
+    }
 
     cadastrar(event) {
         event.preventDefault();
-        console.log(this.foto);
+        let headers = new Headers();
+        headers.append('Content-type', 'application/json');
+        this.http.post('v1/fotos', JSON.stringify(this.foto), {headers: headers})
+            .subscribe(() => {
+                this.foto = new FotoComponent();
+                console.log("Foto armazenada com sucesso.");
+            },
+            erro => console.log(erro));
     }
 
 }
